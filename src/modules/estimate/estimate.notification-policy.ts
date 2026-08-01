adf1207b5e94a37e06a863cc79478c8ecd4edcfa
yoonsojung <mila030403@hansung.ac.kr>
2026-08-01 17:51:14 +0900

    refactor: 파일 제거

--- src/modules/estimate/estimate.notification-policy.ts
+++
@@ -1,6 +0,0 @@
-const REJECTION_NOTIFICATION_VISIBILITY_DAYS = 7;
-const DAY_MS = 24 * 60 * 60 * 1000;
-
-export function getRejectionNotificationExpiresAt(createdAt: Date): Date {
-  return new Date(createdAt.getTime() + REJECTION_NOTIFICATION_VISIBILITY_DAYS * DAY_MS);
-}
